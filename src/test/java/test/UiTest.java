47f2623b53c0fe64df89fb9cfc48f15997e52e72
MukundhanTringapps <mukundhan.k@tringapps.com>
2024-05-26 17:13:13 +0530

    Added Browser parameter in the pom.xml file for parameterized test

--- src/test/java/test/UiTest.java
+++ src/test/java/test/UiTest.java
@@ -2,17 +2,32 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class UiTest {
 
+	private static WebDriver driver;
+
+	@Parameters("Browser")
 	@Test
-	public static void testMethod() {
-		WebDriver driver = new ChromeDriver();
+	public static void testMethod(String browserValue) {
+
+		if(browserValue.equalsIgnoreCase("chrome")){
+			driver = new ChromeDriver();
+			System.out.println("Current driver invoked is Chrome");
+		}
+
+		else if(browserValue.equalsIgnoreCase("edge")){
+			driver = new EdgeDriver();
+			System.out.println("Current driver invoked is Edge");
+		}
+		
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
 		Assert.assertTrue(driver.getTitle().equals("Google"));
 		driver.quit();
 	}
-} 
+}
